9b33fd40cad0077c7b5903de1d7a7b126410577d
Vsevolod <cerneavschivsevolod@gmail.com>
2021-02-26 16:58:48 +0100

    Password generator finito.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,5 @@
 var nome = prompt("Quale è il tuo nome?");
 var cognome = prompt("Quale è il tuo cognome?");
-var anni = prompt("Quanti anni hai?");
 var colorePreferito = prompt("Quale è il tuo colore preferito?");
 
-document.getElementById('nomeCognomeColore').innerHTML = nome + ' ' + cognome + ' ' + anni + ' ' + colorePreferito;
+document.getElementById('nomeCognomeColore').innerHTML = nome + cognome + colorePreferito + '21';
