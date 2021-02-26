2ca60de6268549e17f90bda480676b39091782bb
Vsevolod <cerneavschivsevolod@gmail.com>
2021-02-26 16:49:39 +0100

    Esercizio Finito.

---
+++ assets/js/main.js
@@ -0,0 +1,6 @@
+var nome = prompt("Quale è il tuo nome?");
+var cognome = prompt("Quale è il tuo cognome?");
+var anni = prompt("Quanti anni hai?");
+var colorePreferito = prompt("Quale è il tuo colore preferito?");
+
+document.getElementById('nomeCognomeColore').innerHTML = nome + ' ' + cognome + ' ' + anni + ' ' + colorePreferito;
